97e6664fd5b857a4c4bc162af8e071cc6f584daa
Youngjin Choi <amateur.toss@gmail.com>
2021-06-26 22:43:39 +0900

    fix convention

--- app/frontend/src/index.tsx
+++ app/frontend/src/index.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
 
-const element = <React.StrictMode>
-                  <App/>
-                </React.StrictMode>;
+const element =
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>;
 
 ReactDOM.render(element, document.getElementById('root'));
