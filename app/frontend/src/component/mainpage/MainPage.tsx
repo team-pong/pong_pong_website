18096f3c098e11033df7a009f46fb80554e6d3ca
Youngjin Choi <amateur.toss@gmail.com>
2021-06-26 22:45:52 +0900

    fix fetch error

--- app/frontend/src/component/mainpage/MainPage.tsx
+++ app/frontend/src/component/mainpage/MainPage.tsx
@@ -3,18 +3,16 @@ import { useEffect } from "react";
 const MainPage = (): JSX.Element => {
 
   useEffect(() => {
-    let searchParams = new URLSearchParams(window.location.search);
+    let searchParams: URLSearchParams = new URLSearchParams(window.location.search);
     const fetchOption = {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
       },
-      body: JSON.stringify({code: searchParams.get('code')})
+      body: JSON.stringify({ code: searchParams.get('code') })
     }
-    fetch('http://localhost:3001/api/oauth', fetchOption)
-    .then(res => res.json())
-    .then(res => console.log(res));
+    fetch('http://localhost:3001/api/oauth', fetchOption);
   }, []);
 
   return (
