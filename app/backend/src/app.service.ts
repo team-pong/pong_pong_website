76a963e77b994ab2c2fed758d2cd3a673982a50f
jinwkim <jinwoo700@gmail.com>
2021-06-26 18:12:06 +0900

    Add 42API OAuth token

--- app/backend/src/app.service.ts
+++ app/backend/src/app.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { LoginCodeDto } from './dto/login-token-dto';
+import { CLIENT_ID, CLIENT_SECRET } from './config/config.json';
 import axios, { AxiosResponse } from 'axios';
-import { CLIENT_ID, CLIENT_SECRET } from './config/config.json'
+const qs = require('querystring')
 
 
 @Injectable()
@@ -13,23 +14,41 @@ export class AppService {
   public async getUserInfo(loginCodeDto: LoginCodeDto) {
     // 42api로 해당 코드를 전송하고 토큰 만들어서 반환함
     const { code } = loginCodeDto;
-    const type : string = "autorization_code";
+    const type : string = "authorization_code";
+    console.log(`code ${code}`)
     const getTokenUrl: string = "https://api.intra.42.fr/oauth/token";
-    const request = {
-      type,
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
-      code,
-    };
-    const response: AxiosResponse = await axios.post(getTokenUrl, request, {
-      headers: {
-        accept: 'application/json',
+    const redirectUrl: string = "http://127.0.0.1:3000/mainpage"
+    const options = {
+      uri: getTokenUrl,
+      method: 'POST',
+      qs: {
+        grant_type: type,
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        code: code,
+        redirect_uri: redirectUrl
       },
-    });
-    if (response.data.error) {
-      return console.log("인증 실패함");
+    };
+    const requestBody = {
+      grant_type: type,
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        code: code,
+        redirect_uri: redirectUrl
+    };
+    const config = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
     }
-    const { access_token } = response.data;
-    console.log(response.data);
+    axios.post(getTokenUrl, qs.stringify(requestBody), config)
+      .then(result => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    // const { access_token } = response.data;
+    // console.log(response.data);
   }
 }
