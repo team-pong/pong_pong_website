e68f9c60e4e37fff709088f444c0a19a6962fb9a
jinwkim <jinwoo700@gmail.com>
2021-06-26 16:55:58 +0900

    Add 42api get token func

--- app/backend/src/app.controller.ts
+++ app/backend/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LoginCodeDto } from './dto/login-token-dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,17 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post("/api/oauth/")
+  public async get42UserInfo(@Body() loginCodeDto: LoginCodeDto) {
+    const user = await this.appService.getUserInfo(loginCodeDto);
+    return {
+      status: 200,
+      message: '유저 정보 조회 완료',
+      data: {
+        user,
+      },
+    };
   }
 }
