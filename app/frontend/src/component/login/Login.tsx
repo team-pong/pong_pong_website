4f978b26d3cd7a0abb0e4524b7278a9d241f44f3
Youngjin Choi <yochoi@c1r5s7.42seoul.kr>
2021-06-27 12:51:42 +0900

    fix convention

--- app/frontend/src/component/login/Login.tsx
+++ app/frontend/src/component/login/Login.tsx
@@ -1,14 +1,11 @@
 const Login = (): JSX.Element => {
-
   const UID: string = `1cbdbdbda9ab3cee5773541a19177fa2ae63ae10751dcf3c40466d9a1f0e3f9f`;
 
   const loginOnClick = () => {
-    window.location.href = `https://api.intra.42.fr/oauth/authorize?client_id=${UID}&redirect_uri=http%3A%2F%2F127.0.0.1%3A3000%2Fmainpage&response_type=code`
-  }
+    window.location.href = `https://api.intra.42.fr/oauth/authorize?client_id=${UID}&redirect_uri=http%3A%2F%2F127.0.0.1%3A3000%2Fmainpage&response_type=code`;
+  };
 
-  return (
-    <button onClick={loginOnClick}>LOGIN</button>
-  );
-}
+  return <button onClick={loginOnClick}>LOGIN</button>;
+};
 
 export default Login;
