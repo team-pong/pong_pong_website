e98bd88b0a65215db078ef031dba3eb930f7eeb5
Youngjin Choi <yochoi@c1r5s7.42seoul.kr>
2021-06-27 17:06:38 +0900

    style: change loginpage css

--- app/frontend/src/component/login/Login.tsx
+++ app/frontend/src/component/login/Login.tsx
@@ -1,3 +1,5 @@
+import "/src/css/Login.css";
+
 const Login = (): JSX.Element => {
   const UID: string = `1cbdbdbda9ab3cee5773541a19177fa2ae63ae10751dcf3c40466d9a1f0e3f9f`;
 
@@ -5,7 +7,11 @@ const Login = (): JSX.Element => {
     window.location.href = `https://api.intra.42.fr/oauth/authorize?client_id=${UID}&redirect_uri=http%3A%2F%2F127.0.0.1%3A3000%2Fmainpage&response_type=code`;
   };
 
-  return <button onClick={loginOnClick}>LOGIN</button>;
+  return (
+    <section id="loginSection">
+     <button id="button" onClick={loginOnClick}>LOGIN</button>
+    </section>
+  );
 };
 
 export default Login;
