6dee36b9de8685084a3c55946e59f41d94c8a714
Hna <ghnruf@naver.com>
2021-06-27 16:05:26 +0900

    로그인 후 DB에 저장하는 함수 추가, 환경변수 사용하도록 수정

--- app/backend/src/app.service.ts
+++ app/backend/src/app.service.ts
@@ -2,8 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { LoginCodeDto } from './dto/login-token-dto';
 import { CLIENT_ID, CLIENT_SECRET } from './config/config.json';
 import axios, { AxiosResponse } from 'axios';
+import { Client } from 'pg';
 const qs = require('querystring');
 
+const client = new Client({
+	user: process.env.PG_PONG_ADMIN,
+	host: process.env.PG_HOST,
+	database: process.env.PG_PONG_DB,
+	password: process.env.PG_PONG_PW,
+	port: process.env.PG_PORT,
+});
 
 @Injectable()
 export class AppService {
@@ -20,12 +28,23 @@ export class AppService {
     const { access_token } = result.data;
     
     const data = await getInfo(access_token)
-    const { usual_full_name } = data.data
+		saveInfo(data.data);
 
-    console.log(usual_full_name)
+    // console.log(usual_full_name)
     // const { access_token } = response.data;
     // console.log(response.data);
   }
+}
+
+export async function saveInfo(info) {
+	client.connect();
+	// if DB에 존재하지 않는 사용자면 저장 후 세션생성, 아니면 그냥 세션 생성
+	client.query('INSERT INTO users(user_id, user_image_url) VALUES($1, $2);', 
+		[info.login, info.image_url], 
+		(err, res) => {
+			console.log(err, res)
+			client.end()
+	})
 }
 
 export async function getInfo(access_token) {
