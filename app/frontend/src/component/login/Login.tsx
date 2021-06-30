1f7a349e12cbf6119e6d9b25380058e5fa982943
Youngjin Choi <amateur.toss@gmail.com>
2021-06-30 15:58:38 +0900

    SIMPLIFY 42api redirection address

--- app/frontend/src/component/login/Login.tsx
+++ app/frontend/src/component/login/Login.tsx
@@ -4,12 +4,15 @@ const Login = (): JSX.Element => {
   const UID: string = `1cbdbdbda9ab3cee5773541a19177fa2ae63ae10751dcf3c40466d9a1f0e3f9f`;
 
   const loginOnClick = () => {
-    window.location.href = `https://api.intra.42.fr/oauth/authorize?client_id=${UID}&redirect_uri=http%3A%2F%2F127.0.0.1%3A3000%2Fmainpage&response_type=code`;
+    window.location.href = `https://api.intra.42.fr/oauth/authorize`
+      + `?client_id=${UID}`
+      + `&redirect_uri=http%3A%2F%2F127.0.0.1%3A3000%2Fmainpage`
+      + `&response_type=code`;
   };
 
   return (
     <section id="loginSection">
-     <button id="button" onClick={loginOnClick}>LOGIN</button>
+      <button id="button" onClick={loginOnClick}>LOGIN</button>
     </section>
   );
 };
