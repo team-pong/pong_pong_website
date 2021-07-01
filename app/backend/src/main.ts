f20ca39a68bdf6ad5d08649b5bd7f63064b4a20f
Hangyeol Na <hna@c1r3s4.42seoul.kr>
2021-07-01 17:32:34 +0900

    Fix | CORS 문제 때문에 백엔드에서 쿠키를 못받는 문제 해결

--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -6,7 +6,12 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    "origin": ["http://localhost:3000", "http://127.0.0.1:3000"],
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  });
   app.use(
     session({
       secret: 'pong-secret',
