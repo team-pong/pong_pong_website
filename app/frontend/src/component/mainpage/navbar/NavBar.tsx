bfa8fea0fc874c5e9c9b9809e5f1978914df4c10
donglee <zuzudnf@gmail.com>
2021-07-05 23:32:15 +0900

    Fix NavBar
    usecase:
    1. 친구리스트에서 친구를 클릭해서 컨텍스트메뉴를 보이게 한 이후 사이드메뉴를 닫으면 그대로 남아있음
    -> 컨텍스트메뉴가 보이는 상태에서 사이드메뉴가 닫히면 전부 닫히게 수정함
    2. 컨텍스트메뉴를 연 상태에서 스크롤을 내리면 컨텍스트메뉴는 그 자리에 있고 스크롤이 내려감
    -> 컨텍스트 메뉴를 연 상태에서는 사이드메뉴 스크롤바가 작동하지 않게 수정함.

--- app/frontend/src/component/mainpage/navbar/NavBar.tsx
+++ app/frontend/src/component/mainpage/navbar/NavBar.tsx
@@ -3,73 +3,91 @@ import { Modal, ConfigContent } from "../../modal/Modal";
 import "/src/scss/NavBar.scss";
 
 interface navBarProps {
-  avartarImgUrl: string,
-  friends: { name: string, state: string, avatarURL: string }[]
+  avartarImgUrl: string; //우상단 본인 url 이미지
+  friends: { name: string; state: string; avatarURL: string }[];
+  //avatarURL: 친구들 각각의 아바타이미지
 }
 
 const NavBar = (props: navBarProps): JSX.Element => {
-
   let targetUser = "";
 
   const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
   const [isConfigOpen, setIsConfigOpen] = useState(false);
 
   const showFriendContextMenu = (e: MouseEvent, target: string) => {
-    const contextMenu = document.getElementById('friendcontextmenu');
-    //왜 쿼리실렉터는 style이 안되는데?
+    const contextMenu = document.getElementById("friendcontextmenu");
+
     targetUser = target;
-    //마치 state인것처럼 클릭했을 때 그 클릭한 친구의 이름을 가져오기 위함
-    if (contextMenu.style.display === 'none' || !contextMenu.style.display) {
-      contextMenu.style.display = 'block';
-      contextMenu.style.top = e.pageY + 'px';
-      contextMenu.style.left = e.pageX + 'px';
+    if (contextMenu.style.display === "none" || !contextMenu.style.display) {
+      contextMenu.style.display = "block";
+      contextMenu.style.top = e.pageY + "px";
+      contextMenu.style.left = e.pageX + "px";
+      document.getElementById("sidemenu").style.overflow = "hidden";
     } else {
-      contextMenu.style.display = 'none';
+      contextMenu.style.display = "none";
+      document.getElementById("sidemenu").style.removeProperty("overflow");
       targetUser = "";
     }
-  }
+  };
 
   const sendMessage = (e: MouseEvent) => {
-    console.log('sendMessage', targetUser);
-  }
+    console.log("sendMessage", targetUser);
+  };
 
   const deleteFriend = (e: MouseEvent) => {
-    console.log('deleteFriend', targetUser);
-  }
+    console.log("deleteFriend", targetUser);
+  };
 
   const detectOutsideOfSideMenu = (event: any) => {
-    if (!document.getElementById("sidemenu").contains(event.target)
-      && !document.getElementById("avartarImg").contains(event.target)) {
+    if (
+      !document.getElementById("sidemenu").contains(event.target) &&
+      !document.getElementById("avartarImg").contains(event.target)
+    ) {
       setIsSideMenuOpen(false);
-      document.getElementById('friendcontextmenu').style.display = "none";
+      document.getElementById("friendcontextmenu").style.display = "none";
       targetUser = "";
     }
-  }
+  };
 
   useEffect(() => {
-    if (isSideMenuOpen) document.getElementById("sidemenu").style.width = "250px";
-    else document.getElementById("sidemenu").style.width = "0px";
+    if (isSideMenuOpen) {
+      document.getElementById("sidemenu").style.width = "250px";
+    } else {
+      document.getElementById("friendcontextmenu").style.display = "none";
+      document.getElementById("sidemenu").style.width = "0px";
+    }
   }, [isSideMenuOpen]);
 
   useEffect(() => {
     document.addEventListener("mousedown", detectOutsideOfSideMenu);
-    return () => document.removeEventListener("mousedown", detectOutsideOfSideMenu);
+    return () =>
+      document.removeEventListener("mousedown", detectOutsideOfSideMenu);
   }, []);
 
+  //semantic tag <nav> 사용함
   return (
-    <>
+    <nav>
       <div className="navbar">
         <ul>
           <li><a href="">Match Making</a></li>
           <li><a href="">History</a></li>
           <li><a href="">Chat</a></li>
         </ul>
-        <img src={props.avartarImgUrl} id="avartarImg" alt="Avatar" onClick={() => setIsSideMenuOpen(!isSideMenuOpen)} />
-      </div >
+        <img
+          src={props.avartarImgUrl}
+          id="avartarImg"
+          alt="Avatar"
+          onClick={() => setIsSideMenuOpen(!isSideMenuOpen)}
+        />
+      </div>
       <div id="sidemenu">
         <ul id="friendlist">
           {props.friends.map((friend, i: number) => (
-            <li onClick={(e) => showFriendContextMenu(e, friend.name)} key={i}>
+            <li
+              id={`friend-list-${friend.name}`}
+              onClick={(e) => showFriendContextMenu(e, friend.name)}
+              key={friend.name}
+            >
               <img src={friend.avatarURL} /> {friend.name}/{friend.state}
             </li>
           ))}
@@ -78,11 +96,19 @@ const NavBar = (props: navBarProps): JSX.Element => {
           <li onClick={sendMessage}>DM</li>
           <li onClick={deleteFriend}>DELETE</li>
         </ul>
-        <img src="./public/config.png" alt="Config" onClick={() => setIsConfigOpen(true)} />
-        <Modal content={ConfigContent} display={isConfigOpen} handleClose={() => setIsConfigOpen(false)} />
+        <img
+          src="./public/config.png"
+          alt="Config"
+          onClick={() => setIsConfigOpen(true)}
+        />
+        <Modal
+          content={ConfigContent}
+          display={isConfigOpen}
+          handleClose={() => setIsConfigOpen(false)}
+        />
       </div>
-    </>
+    </nav>
   );
-}
+};
 
 export default NavBar;
