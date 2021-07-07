e5e48f69435eba5a64977e22e618f5e41e283d1c
AMATEUR_TOSS <amateur.toss@gmail.com>
2021-07-07 23:47:54 +0900

    Add mainPage Button
    mainPage 에 Record, Chat, Match Making 버튼 추가

--- app/frontend/src/component/mainpage/MainPage.tsx
+++ app/frontend/src/component/mainpage/MainPage.tsx
@@ -5,6 +5,10 @@ import { testFriendList } from './dummyData'
 
 const MainPage = (): JSX.Element => {
 
+  const [isRecordOpen, setIsRecordOpen] = useState(false); // 전적 모달을 위한 State
+  const [isChatOpen, setIsChatOpen] = useState(false); // 채팅 모달을 위한 State
+  const [isGameOpen, setIsGameOpen] = useState(false); // 게임, 매치메이킹 모달을 위한 State
+
   useEffect(() => {
 
     const postAuthCodeToBackend = async () => {
@@ -35,6 +39,12 @@ const MainPage = (): JSX.Element => {
       <NavBar
         avartarImgUrl="https://static.coindesk.com/wp-content/uploads/2021/04/dogecoin.jpg"
         friends={testFriendList} />
+      <button onClick={() => setIsRecordOpen(true)}>Record</button>
+      <button onClick={() => setIsChatOpen(true)}>Chat</button>
+      <button onClick={() => setIsGameOpen(true)}>Match Making</button>
+      <Modal content={() => <h1>Record</h1>} display={isRecordOpen} handleClose={() => setIsRecordOpen(false)}/>
+      <Modal content={() => <h1>Chat</h1>} display={isChatOpen} handleClose={() => setIsChatOpen(false)}/>
+      <Modal content={() => <h1>Game</h1>} display={isGameOpen} handleClose={() => setIsGameOpen(false)}/>
     </>
   );
 }
