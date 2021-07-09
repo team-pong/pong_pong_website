380c1caaefc05aa62f8f97b04ff5b852ea1abbc3
Choi Young-jin <amateur.toss@gmail.com>
2021-07-09 13:25:40 +0900

    Add Main page button (#15)
    Main Page에 버튼 추가

--- app/frontend/src/component/mainpage/MainPage.tsx
+++ app/frontend/src/component/mainpage/MainPage.tsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from "react";
 import { Modal } from '../modal/Modal'
 import NavBar from './navbar/NavBar'
+import '/src/scss/MainPage.scss'
 import EasyFetch from './../../utils/EasyFetch';
 import { testFriendList } from './dummyData'
 
 const MainPage = (): JSX.Element => {
 
-  const [modalDisplay, setModalDisplay] = useState(false);
+  const [isRecordOpen, setIsRecordOpen] = useState(false); // 전적 모달을 위한 State
+  const [isChatOpen, setIsChatOpen] = useState(false); // 채팅 모달을 위한 State
+  const [isGameOpen, setIsGameOpen] = useState(false); // 게임, 매치메이킹 모달을 위한 State
 
   useEffect(() => {
 
@@ -28,8 +31,18 @@ const MainPage = (): JSX.Element => {
       <NavBar
         avartarImgUrl="https://static.coindesk.com/wp-content/uploads/2021/04/dogecoin.jpg"
         friends={testFriendList} />
-      <button onClick={() => setModalDisplay(!modalDisplay)}>모달 display test</button>
-      <Modal content={() => <h1>content</h1>} display={modalDisplay} handleClose={() => setModalDisplay(false)} />
+      <button id="record" onClick={() => setIsRecordOpen(true)}>
+        <img src="https://img.icons8.com/ios/50/000000/-scoreboard-.png"/>
+      </button>
+      <button id="chat" onClick={() => setIsChatOpen(true)}>
+        <img src="https://img.icons8.com/ios/50/000000/chat--v1.png"/>
+      </button>
+      <button id="game" onClick={() => setIsGameOpen(true)}>
+        <img src="https://img.icons8.com/ios/50/000000/head-to-head.png"/>
+      </button>
+      <Modal content={() => <h1>Record</h1>} display={isRecordOpen} handleClose={() => setIsRecordOpen(false)}/>
+      <Modal content={() => <h1>Chat</h1>} display={isChatOpen} handleClose={() => setIsChatOpen(false)}/>
+      <Modal content={() => <h1>Game</h1>} display={isGameOpen} handleClose={() => setIsGameOpen(false)}/>
     </>
   );
 }
