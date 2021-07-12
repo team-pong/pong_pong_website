e7c00724ed5972b5a2a78f523e22824942cf936d
DomMorello <zuzudnf@gmail.com>
2021-07-12 15:56:51 +0900

    REFACTOR: Nav bar new design 구현했습니다. (#22)
    REFACTOR NavBar가 항상 좌측에 보여지고 MainPage에 세 개의 버튼이 있도록 수정함
    
    MainPage 컴포넌트에서 stateSetter를 NavBar 컴포넌트에 props로 줘서
    NavBar컴포넌트에서 요소를 클릭했을 때 MainPage 컴포넌트 위에 모달이 뜨도록 구현함
    css media 적용해서 화면이 작아졌을 때 (넓이 400px ~ 900px)에서 NavBar 변경되고
    (넓이 400px 이하)에서 NavBar 사라지도록 구현함.
    
    TODO: MainPage 화면에 보이는 세 개의 버튼도 media를 적용하면 더 좋을 것 같습니다.
    
    Co-authored-by: AMATEUR_TOSS <amateur.toss@gmail.com>

--- app/frontend/src/component/mainpage/navbar/NavBar.tsx
+++ app/frontend/src/component/mainpage/navbar/NavBar.tsx
@@ -1,112 +1,70 @@
-import { MouseEvent, useEffect, useState } from "react";
-import { ModalController, ConfigContent } from "../../modal/Modal";
+import { MouseEvent } from "react";
 import "/src/scss/NavBar.scss";
 
+/*!
+ * @author donglee
+ * @brief 좌측에 NavBar가 상시 나타나있음
+ *        부모 컴포넌트인 MainPage에서 props로 stateSetter를 받아와서
+ *        이 컴포넌트에서 부모컴포넌트의 state를 수정해서 NavBar 요소 선택시
+ *        부모 컴포넌트 위에서 모달이 뜨도록 함.
+ * @param[in] avartarImgUrl: NavBar에 나타나는 본인의 아바타이미지url
+ * @param[in] friends: 테스트용 친구목록 TODO: 모달에서 보여지게 해야 함
+ * @param[in] setIsRecordOpen: 클릭시 Record 모달이 오픈되도록 하기 위한 Mainpage.tsx의 stateSetter
+ * @param[in] setIsGameOpen: 클릭시 Match-game 모달이 오픈되도록 하기 위한 Mainpage.tsx의 stateSetter
+ * @param[in] setIsConfigOpen: 클릭시 Config 모달이 오픈되도록 하기 위한 Mainpage.tsx의 stateSetter
+ */
+
 interface navBarProps {
-  avartarImgUrl: string; //우상단 본인 url 이미지
+  avartarImgUrl: string;
   friends: { name: string; state: string; avatarURL: string }[];
-  //avatarURL: 친구들 각각의 아바타이미지
+  setIsRecordOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsGameOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsConfigOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const NavBar = (props: navBarProps): JSX.Element => {
   let targetUser = "";
 
-  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
-  const [isConfigOpen, setIsConfigOpen] = useState(false);
+  /* LEGACY: 컨텍스트 메뉴를 나중에 활용할 때 쓰일 코드 일단 주석처리 */
+  
+  // const showFriendContextMenu = (e: MouseEvent, target: string) => {
+  //   const contextMenu = document.getElementById("friendcontextmenu");
 
-  const showFriendContextMenu = (e: MouseEvent, target: string) => {
-    const contextMenu = document.getElementById("friendcontextmenu");
+  //   targetUser = target;
+  //   if (contextMenu.style.display === "none" || !contextMenu.style.display) {
+  //     contextMenu.style.display = "block";
+  //     contextMenu.style.top = e.pageY + "px";
+  //     contextMenu.style.left = e.pageX + "px";
+  //     document.getElementById("sidemenu").style.overflow = "hidden";
+  //   } else {
+  //     contextMenu.style.display = "none";
+  //     document.getElementById("sidemenu").style.removeProperty("overflow");
+  //     targetUser = "";
+  //   }
+  // };
 
-    targetUser = target;
-    if (contextMenu.style.display === "none" || !contextMenu.style.display) {
-      contextMenu.style.display = "block";
-      contextMenu.style.top = e.pageY + "px";
-      contextMenu.style.left = e.pageX + "px";
-      document.getElementById("sidemenu").style.overflow = "hidden";
-    } else {
-      contextMenu.style.display = "none";
-      document.getElementById("sidemenu").style.removeProperty("overflow");
-      targetUser = "";
-    }
-  };
+  // const sendMessage = (e: MouseEvent) => {
+  //   console.log("sendMessage", targetUser);
+  // };
 
-  const sendMessage = (e: MouseEvent) => {
-    console.log("sendMessage", targetUser);
-  };
-
-  const deleteFriend = (e: MouseEvent) => {
-    console.log("deleteFriend", targetUser);
-  };
-
-  const detectOutsideOfSideMenu = (event: any) => {
-    if (
-      !document.getElementById("sidemenu").contains(event.target) &&
-      !document.getElementById("avartarImg").contains(event.target)
-    ) {
-      setIsSideMenuOpen(false);
-      document.getElementById("friendcontextmenu").style.display = "none";
-      targetUser = "";
-    }
-  };
-
-  useEffect(() => {
-    if (isSideMenuOpen) {
-      document.getElementById("sidemenu").style.width = "250px";
-    } else {
-      document.getElementById("friendcontextmenu").style.display = "none";
-      document.getElementById("sidemenu").style.width = "0px";
-    }
-  }, [isSideMenuOpen]);
-
-  useEffect(() => {
-    document.addEventListener("mousedown", detectOutsideOfSideMenu);
-    return () =>
-      document.removeEventListener("mousedown", detectOutsideOfSideMenu);
-  }, []);
+  // const deleteFriend = (e: MouseEvent) => {
+  //   console.log("deleteFriend", targetUser);
+  // };
 
   //semantic tag <nav> 사용함
+  //TODO: Users 클릭시 친구 목록이 보여야 함. 테스트용으로 Config 모달 뜨게 해 놓음.
   return (
-    <nav>
-      <div className="navbar">
-        <ul>
-          <li><a href="">Match Making</a></li>
-          <li><a href="">History</a></li>
-          <li><a href="">Chat</a></li>
-        </ul>
-        <img
-          src={props.avartarImgUrl}
-          id="avartarImg"
-          alt="Avatar"
-          onClick={() => setIsSideMenuOpen(!isSideMenuOpen)}
-        />
-      </div>
-      <div id="sidemenu">
-        <ul id="friendlist">
-          {props.friends.map((friend, i: number) => (
-            <li
-              id={`friend-list-${friend.name}`}
-              onClick={(e) => showFriendContextMenu(e, friend.name)}
-              key={friend.name}
-            >
-              <img src={friend.avatarURL} /> {friend.name}/{friend.state}
-            </li>
-          ))}
-        </ul>
-        <ul id="friendcontextmenu">
-          <li onClick={sendMessage}>메세지보내기</li>
-          <li onClick={deleteFriend}>친구삭제</li>
-        </ul>
-        <img
-          src="./public/config.png"
-          alt="Config"
-          onClick={() => setIsConfigOpen(true)}
-        />
-        <ModalController
-          content={ConfigContent}
-          display={isConfigOpen}
-          closer={() => setIsConfigOpen(false)}
-        />
-      </div>
+    <nav className="menu">
+      <header className="avatar">
+        <img id="avartarImg" src={props.avartarImgUrl} alt="Avatar" />
+        <h2>DomHardy</h2>
+      </header>
+      <ul>
+        <li onClick={() => props.setIsConfigOpen(true)} className="icon-users"><span>Users</span></li>
+        <li onClick={() => props.setIsRecordOpen(true)} className="icon-record"><span>Record</span></li>
+        <li onClick={() => props.setIsGameOpen(true)} className="icon-match-game"><span>Match Game</span></li>
+        <li onClick={() => props.setIsConfigOpen(true)} className="icon-settings"><span>Settings</span></li>
+      </ul>
     </nav>
   );
 };
