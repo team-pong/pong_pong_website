9cb4b156e3019a9208def0d7a249d7529f83a9a2
DomMorello <zuzudnf@gmail.com>
2021-07-15 00:04:15 +0900

    FEAT: 모달창 뒤로가기 구현(+모달창 닫힐 때 애니메이션 구현) #32 (#34)
    * REFACTOR 모달 창이 닫힐 때 애니메이션 효과를 적용함
    
    * FEAT 모달 창에서 뒤로가기 클릭시 모달창만 꺼지도록 기능 구현

--- app/frontend/src/component/modal/Modal.tsx
+++ app/frontend/src/component/modal/Modal.tsx
@@ -1,30 +1,29 @@
 import React, { FC, useEffect } from "react";
 import "/src/scss/Modal.scss";
-
 import ChatContent from './content/ChatContent';
-import ConfigContent from './content/ConfigContent'
+import ConfigContent from './content/ConfigContent';
 
 /*!
- * @author yochoi
+ * @author yochoi, donglee
  * @brief display 에 맞춰 Modal 컴포넌트를 마운트, 언마운트 해주는 FC
  * @param[in] content: Modal 안에 들어갈 FC
  * @param[in] display: Modal 의 display 여부
- * @param[in] closer: Modal 의 상위 컴포넌트에서 display를 제어해줄 함수
+ * @param[in] stateSetter: Modal의 상위 컴포넌트 state를 조정함으로써 display를 결정함
  */
 
 interface modalControllerProps {
   content: FC;
   display: boolean;
-  closer: () => void;
+  stateSetter: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const ModalController: FC<modalControllerProps> = ({
   content,
   display,
-  closer,
+  stateSetter,
 }): JSX.Element => {
   if (display) {
-    return <Modal content={content} closer={closer} />;
+    return <Modal content={content} stateSetter={stateSetter} />;
   } else {
     return <></>;
   }
@@ -34,15 +33,27 @@ const ModalController: FC<modalControllerProps> = ({
  * @author yochoi
  * @brief FC를 Modal 형태로 띄워주는 컴포넌트
  * @param[in] content: Modal 안에 들어갈 함수형 컴포넌트
- * @param[in] closer: Modal 의 상위 컴포넌트에서 display를 제어해줄 함수
+ * @param[in] stateSetter: Modal의 상위 컴포넌트 state를 조정함으로써 display를 결정함
  */
 
 interface modalPros {
   content: FC;
-  closer: () => void;
+  stateSetter: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const Modal: FC<modalPros> = ({ content, closer }): JSX.Element => {
+const Modal: FC<modalPros> = ({ content, stateSetter }): JSX.Element => {
+  let isGoBackClicked = false;  //뒤로가기 버튼을 눌렀는지 여부를 저장
+  
+  /*!
+  * @author donglee
+  * @brief 모달 컴포넌트를 종료할 때 실행하는 함수.
+  *       애니메이션 효과가 끝나는 시간 동안 기다렸다가 stateSetter(false)를 통해 컴포넌트 언마운트시킴
+  */
+  const closer = () => {
+    setTimeout(() => { stateSetter(false); }, 700);
+    hideAnimatedModal();
+  }
+  
   const detectOutsideOfModal = (e: React.MouseEvent) => {
     e.preventDefault();
     if (e.target === e.currentTarget) {
@@ -62,7 +73,7 @@ const Modal: FC<modalPros> = ({ content, closer }): JSX.Element => {
 
   /*!
   * @author donglee
-  * @brief 모달 컴포넌트 style만 바꿔주는 animation 함수
+  * @brief 모달 컴포넌트 나타날 때 style만 바꿔주는 animation 함수
   */
   const showAnimatedModal = () => {
     const modal = document.getElementById("modal");
@@ -76,10 +87,50 @@ const Modal: FC<modalPros> = ({ content, closer }): JSX.Element => {
     content.style.transform = "scale(1)";
   };
 
+  /*!
+  * @author donglee
+  * @brief 모달 컴포넌트 사라질 때 style만 바꿔주는 animation 함수
+  */
+  const hideAnimatedModal = () => {
+    const modal = document.getElementById("modal");
+    const content = document.getElementById("content");
+
+    modal.style.opacity = "0";
+    modal.style.transition = "opacity 250ms 500ms ease";
+    content.style.opacity = "0";
+    content.style.transform = "scale(0.6)";
+    content.style.transition = "opacity 250ms 250ms ease, transform 300ms 250ms ease";
+  };
+
+  /*!
+  * @author donglee
+  * @brief 모달 컴포넌트에서 브라우저 뒤로가기 버튼을 눌렀을 때
+  *        이전 history로 이동하는 것을 방지하고 단순히 모달 컴포넌트만 닫힘
+  */
+  const goBack = () => {
+    isGoBackClicked = true;
+    closer();
+  };
+
+  /*!
+  * @author donglee
+  * @brief -history에 새로운 state를 넣어서 뒤로가기시에 모달창만 꺼지도록함
+  *        -SEC키와 뒤로가기 버튼에 대한 이벤트리스너를 등록하고 언마운트시에 제거함
+  */
   useEffect(() => {
+    history.pushState({page:"modal"}, document.title);
     showAnimatedModal();
+
     document.addEventListener("keyup", detectESC);
-    return () => document.removeEventListener("keyup", detectESC);
+    window.addEventListener("popstate", goBack);
+    return () => {
+      document.removeEventListener("keyup", detectESC);
+      window.removeEventListener("popstate", goBack);
+      //뒤로가기를 누르지 않고 종료할 경우엔 state가 새로 있으니 뒤로가기를 해줘야함.
+      if (!isGoBackClicked) {
+        history.back();
+      }
+    };
   }, []);
 
   return (
