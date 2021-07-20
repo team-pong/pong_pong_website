fbd83ded9213e316ebc2ce4301b414e1c841bcae
hna <ghnruf@naver.com>
2021-07-20 21:29:56 +0900

    Fix | 명령 순서 수정

--- app/db/init-user-db.sh
+++ app/db/init-user-db.sh
@@ -4,8 +4,8 @@ chown postgres:postgres postgres
 # @brief postgres 디렉토리 초기화
 # @todo 1. postgres 폴더가 비어있는 경우에만 아래 스크립트가 실행되도록 바꿔야함
 initdb /db/postgres;
-pg_ctl -D /db/postgres -l logfile start;
 echo 'host    all             all             0.0.0.0/0               trust' >> /db/postgres/pg_hba.conf
+pg_ctl -D /db/postgres -l logfile start;
 
 psql <<- EOSQL
     CREATE USER $PG_PONG_ADMIN WITH PASSWORD '$PG_PONG_PW';
