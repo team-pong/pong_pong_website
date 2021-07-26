d1c4dde0117c1dab2f43ea24301f50bbc76ad49d
jinbkim <topmedia3@naver.com>
2021-07-26 18:19:29 +0900

    ADD : TypeORM connection
    creat_initial_data : 초기값 넣는 역할

--- app/backend/src/app.module.ts
+++ app/backend/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as ormconfig from '../ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AchivementsModule } from './achivements/achivements.module';
+import { UsersService } from './users/users.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { MatchModule } from './match/match.module';
+import { ChatModule } from './chat/chat.module';
+import { StatModule } from './stat/stat.module';
+import { BanModule } from './ban/ban.module';
+import { AdminModule } from './admin/admin.module';
+import { MuteModule } from './mute/mute.module';
+import { FriendModule } from './friend/friend.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+		TypeOrmModule.forRoot(ormconfig),
+    AchivementsModule, UsersModule, MatchModule, ChatModule, StatModule, BanModule, AdminModule, MuteModule, FriendModule],
   controllers: [AppController],
   providers: [AppService],
 })
