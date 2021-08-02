10bdf75140577e2649b85d4b7d07a4c918791f56
AMATEUR_TOSS <amateur.toss@gmail.com>
2021-08-02 21:45:38 +0900

    Add search

--- app/frontend/src/component/modal/content/RecordContent.tsx
+++ app/frontend/src/component/modal/content/RecordContent.tsx
@@ -1,8 +1,24 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 const RecordContent: FC = (): JSX.Element => {
+
+  const [nickNameToFind, setNickNameToFind] = useState("");
+
+  const search = () => {
+    if (nickNameToFind) {
+      console.log(`try to find ${nickNameToFind}`)
+    }
+  }
+
   return (
-    <></>
+    <div id="record-content">
+      <input
+        type="text"
+        placeholder="닉네임"
+        value={nickNameToFind}
+        onChange={({target: {value}}) => setNickNameToFind(value)} />
+      <button onClick={search}>Search</button>
+    </div>
   );
 }
 
