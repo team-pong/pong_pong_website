a02f8544a41a49a2c5671541adb431c0011609b5
AMATEUR_TOSS <amateur.toss@gmail.com>
2021-08-07 19:15:48 +0900

    Add ChatContent/Selector radio button

--- app/frontend/src/component/modal/content/ChatContent.tsx
+++ app/frontend/src/component/modal/content/ChatContent.tsx
@@ -11,6 +11,7 @@ import EasyFetch from "../../../utils/EasyFetch";
 const ChatContent: FC = (): JSX.Element => {
 
   const [chatRoomToFind, setChatRoomToFind] = useState("");
+  const [chatRoomSelector, setChatRoomSelector] = useState("all");
 
   return (
     <div id="chat-content">
@@ -24,6 +25,20 @@ const ChatContent: FC = (): JSX.Element => {
           onKeyDown={(e) => {if (e.key === "Enter") null}} />
         <button onClick={null}><img src="./public/search.svg" alt="검색"/></button>
       </div>
+      <ul id="chat-room-selector">
+        <li onClick={() => setChatRoomSelector("all")}>
+            <input type="radio" name="all" checked={chatRoomSelector === "all"} onChange={() => {}}/>
+            <label>전체</label>
+        </li>
+        <li onClick={() => setChatRoomSelector("public")}>
+            <input type="radio" name="public" checked={chatRoomSelector === "public"} onChange={() => {}}/>
+            <label>공개방</label>
+        </li>
+        <li onClick={() => setChatRoomSelector("protected")}>
+            <input type="radio" name="protected" checked={chatRoomSelector === "protected"} onChange={() => {}}/>
+            <label>비밀방</label>
+        </li>
+      </ul>
     </div>
   );
 }
