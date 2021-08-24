b3ec554a58e2262f66c41afbb7db932ae442c24d
donglee <zuzudnf@gmail.com>
2021-08-24 16:50:36 +0900

    FIX SMALL_MODAL 상수를 없애고 scss 파일에서 small modal의 경우 class를 하나 추가해서 작은모달은 props로 smallModal 을 주게끔 수정했습니다

---
+++ app/frontend/src/component/modal/content/ManageFriendContent.tsx
@@ -0,0 +1,18 @@
+import "../../../scss/content/ManageFriendContent.scss";
+
+const ManageFriendContent: React.FC = () => {
+    return (
+    	<div id="whole-container">
+				<div id="friend-list">
+					list1<br/>
+					list2
+				</div>
+				<div id="banned-list">
+					banned1<br/>
+					banned2
+				</div>
+			</div>
+    );
+}
+ 
+export default ManageFriendContent;
