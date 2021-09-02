348f33f769c6f4af5d4ef8ac587369f03b6626e5
AMATEUR_TOSS <amateur.toss@gmail.com>
2021-09-02 14:39:14 +0900

    Add client socket connection

--- app/frontend/src/component/modal/content/game/GameMatchContent.tsx
+++ app/frontend/src/component/modal/content/game/GameMatchContent.tsx
@@ -1,6 +1,12 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import io from "socket.io-client";
 
 const GameMatchContent: FC<{matchType: string}> = ({matchType}): JSX.Element => {
+
+  useEffect(() => {
+    const socket = io("http://127.0.0.1:3001/game");
+  }, []);
+
   return (
     <div id="game-match-content">
       {`매치메이킹중(${matchType})...`}
