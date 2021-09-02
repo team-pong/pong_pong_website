44a36f5711fd1d53f79ae810b4a9201ced5af023
Hna <ghnruf@naver.com>
2021-09-02 19:00:33 +0900

    Add WithCredential for send cookie to websocket server

--- app/frontend/src/component/modal/content/game/GameMatchContent.tsx
+++ app/frontend/src/component/modal/content/game/GameMatchContent.tsx
@@ -4,7 +4,12 @@ import io from "socket.io-client";
 const GameMatchContent: FC<{matchType: string}> = ({matchType}): JSX.Element => {
 
   useEffect(() => {
-    const socket = io("http://127.0.0.1:3001/game");
+    const socket = io("http://127.0.0.1:3001/game", {withCredentials: true});
+    socket.emit(`${matchType}`);
+    socket.on('matched', (data: {roomId: string, opponent: string}) => {
+      console.log(JSON.stringify(data));
+    });
+    return (() => {socket.disconnect();});
   }, []);
 
   return (
