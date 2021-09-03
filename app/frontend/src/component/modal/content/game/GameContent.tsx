1bad8e176e2a275f556f4a0ef3e2b53b65d5f0a0
AMATEUR_TOSS <amateur.toss@gmail.com>
2021-09-03 15:45:53 +0900

    Add game content margin

--- app/frontend/src/component/modal/content/game/GameContent.tsx
+++ app/frontend/src/component/modal/content/game/GameContent.tsx
@@ -2,12 +2,21 @@ import { FC } from "react";
 import { Link, Route } from "react-router-dom";
 import Modal from "../../Modal";
 import GameMatchContent from "./GameMatchContent";
+import "/src/scss/content/game/GameContent.scss";
 
 const GameContent: FC = (): JSX.Element => {
   return (
-    <div id="gmae-content">
-      <Link to="/mainpage/game/normal-matching">일반게임</Link>
-      <Link to="/mainpage/game/ladder-matching">래더게임</Link>
+    <div id="game-content">
+      <Link
+        to="/mainpage/game/normal-matching"
+        className="match-buttons">
+        일반게임
+      </Link>
+      <Link
+        to="/mainpage/game/ladder-matching"
+        className="match-buttons">
+        래더게임
+      </Link>
 
       <Route path="/mainpage/game/normal-matching">
         <Modal id={Date.now()} smallModal content={<GameMatchContent matchType="normal"/>}/>
