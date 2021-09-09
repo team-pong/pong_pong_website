4ac79bc9c03a4e52e55a22806563597c9cfb4c13
AMATEUR_TOSS <amateur.toss@gmail.com>
2021-09-09 17:11:23 +0900

    Fix socket connection

--- app/frontend/src/component/modal/content/game/GameMatchContent.tsx
+++ app/frontend/src/component/modal/content/game/GameMatchContent.tsx
@@ -16,8 +16,8 @@ interface gameMatchContentProps
 const GameMatchContent: FC<gameMatchContentProps> = ({match: {params}, setIsMatched}): JSX.Element => {
 
   useEffect(() => {
-    const socket = io("http://127.0.0.1:3001/game");
-    socket.emit(params.matchType.split('-')[0], {withCredentials: true});
+    const socket = io("http://127.0.0.1:3001/game", {withCredentials: true});
+    socket.emit(params.matchType.split('-')[0]);
     socket.on("matched", (roomId: string, opponent: string) => {
       setIsMatched({isMatched: true, roomId, opponent});
     });
