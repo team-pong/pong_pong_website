1b58af4b72b2531fef7d3570aeea53935daf3e34
AMATEUR_TOSS <amateur.toss@gmail.com>
2021-09-09 16:45:44 +0900

    Add game match socket connection

--- app/frontend/src/component/modal/content/game/GameMatchContent.tsx
+++ app/frontend/src/component/modal/content/game/GameMatchContent.tsx
@@ -1,6 +1,7 @@
-import { Dispatch, FC, SetStateAction } from 'react';
+import { Dispatch, FC, SetStateAction, useEffect } from 'react';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 import Loading from "../../../loading/Loading";
+import { io } from "socket.io-client";
 import "/src/scss/content/game/GameMatchContent.scss";
 
 interface gameMatchContentProps
@@ -13,16 +14,21 @@ interface gameMatchContentProps
 }
 
 const GameMatchContent: FC<gameMatchContentProps> = ({match: {params}, setIsMatched}): JSX.Element => {
+
+  useEffect(() => {
+    const socket = io("http://127.0.0.1:3001/game");
+    socket.emit(params.matchType.split('-')[0], {withCredentials: true});
+    socket.on("matched", (roomId: string, opponent: string) => {
+      setIsMatched({isMatched: true, roomId, opponent});
+    });
+    return (() => {socket.disconnect();});
+  }, []);
+
   return (
     <div id="game-match-content">
       <Loading width={400} height={300} color="#62C375"/>
       {params.matchType === "normal-match" && "일반 게임 찾는중..."}
       {params.matchType === "ladder-match" && "레더 게임 찾는중..."}
-      <button onClick={() => setIsMatched({
-        isMatched: true,
-        roomId: "13",
-        opponent: "아무개"
-      })}>수동 매칭</button>
     </div>
   );
 }
