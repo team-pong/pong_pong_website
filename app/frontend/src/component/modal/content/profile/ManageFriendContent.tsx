4b3f4896deaebdf056aa779355d167a1ca1003d7
DomMorello <zuzudnf@gmail.com>
2021-09-14 23:09:07 +0900

    Feat ManageFriend  block, unblock friend (#160)
    * 친구 차단, 차단해제 구현
    
    * 친구관리 리스트 css 수정, 친구 차단, 차단해제

--- app/frontend/src/component/modal/content/profile/ManageFriendContent.tsx
+++ app/frontend/src/component/modal/content/profile/ManageFriendContent.tsx
@@ -22,8 +22,23 @@ const FriendList: React.FC = () => {
 		console.log(`${nick} delete`);
 	};
 
-	const blockFriend = (nick: string) => {
-		console.log(`${nick} block`);
+  /*!
+   * @author donglee
+   * @brief 친구 차단 POST 요청 후 성공하면 state를 해당 친구를 제거한 상태로 업데이트한다
+   */
+	const blockFriend = async (nick: string) => {
+		const easyfetch = new EasyFetch("http://127.0.0.1:3001/block", "POST");
+		const body = {
+			"block_nick": nick,
+		};
+		const res = await (await easyfetch.fetch(body)).json();
+
+		if (res.err_msg !== "Success") {
+			alert("사용자의 닉네임이 변경됐을 수 있습니다. 친구관리를 끄고 다시 시도하십시오.");
+		} else {
+			const updatedList = friendList.filter((friend) => friend.nick !== nick);
+			setFriendList(updatedList);
+		}
 	};
 
 	const getFriendList = async () => {
@@ -45,7 +60,7 @@ const FriendList: React.FC = () => {
 						<li key={key}>
 							<div className="fl-user-info">
 								<img className="fl-avatar" src={friend.avatar_url} alt="프로필" />
-								<div>
+								<div className="fl-user-info-text">
 									<span className="fl-nickname">{friend.nick}</span>
 									<span className="fl-title">{setAchievementStr(friend.ladder_level)}</span>
 									<img className="fl-title-icon" src={setAchievementImg(friend.ladder_level)} alt="타이틀로고" />
@@ -75,12 +90,20 @@ const FriendList: React.FC = () => {
 	}
 }
 
-const BlockedList: React.FC<{nick: string}> = ({nick}) => {
+const BlockedList: React.FC = () => {
 	
 	const [blockedList, setBlockedList] = useState<Friend[]>();
 
-	const unblockFriend = (nick: string) => {
-		console.log(`${nick} unblock!`);
+	const unblockFriend = async (nick: string) => {
+		const easyfetch = new EasyFetch(`http://127.0.0.1:3001/block?block_nick=${nick}`, "DELETE");
+		const res = await (await easyfetch.fetch()).json();
+		
+		if (res.err_msg !== "Success") {
+			alert("사용자의 닉네임이 변경됐을 수 있습니다. 친구관리를 끄고 다시 시도하십시오.");
+		} else {
+			const updatedList = blockedList.filter((friend) => friend.nick !== nick);
+			setBlockedList(updatedList);
+		}
 	};
 
 	const getBlockedList = async () => {
@@ -102,7 +125,7 @@ const BlockedList: React.FC<{nick: string}> = ({nick}) => {
 						<li key={key}>
 							<div className="fl-user-info">
 								<img className="fl-avatar" src={blocked.avatar_url} alt="프로필" />
-								<div>
+								<div className="fl-user-info-text">
 									<span className="fl-nickname">{blocked.nick}</span>
 									<span className="fl-title">{setAchievementStr(blocked.ladder_level)}</span>
 									<img className="fl-title-icon" src={setAchievementImg(blocked.ladder_level)} alt="타이틀로고" />
@@ -145,7 +168,7 @@ const ManageFriendContent: React.FC<{nick: string}> = ({nick}) => {
 			</div>
 			<div className="selected-list">
 				{!isBlockedSelected && <FriendList />}
-				{isBlockedSelected && <BlockedList nick={nick}/>}
+				{isBlockedSelected && <BlockedList />}
 			</div>
 		</div>
   );
