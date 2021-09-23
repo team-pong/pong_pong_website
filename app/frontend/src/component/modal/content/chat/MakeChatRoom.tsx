4cd30b3a03014de2e7f6170f6b4b3448b537370d
Donghyeon Lee <zuzudnf@gmail.com>
2021-09-23 21:16:43 +0900

    radio failure

--- app/frontend/src/component/modal/content/chat/MakeChatRoom.tsx
+++ app/frontend/src/component/modal/content/chat/MakeChatRoom.tsx
@@ -4,10 +4,10 @@ import EasyFetch from "../../../../utils/EasyFetch";
 
 const MakeChatRoom: FC = (props): JSX.Element => {
 
-  const makeChatRoom = (e) => {
-    e.preventDefault();
-    console.log("cubsuimt1");
-  };
+  // const makeChatRoom = (e) => {
+  //   e.preventDefault();
+  //   console.log("cubsuimt1");
+  // };
 
   // const test = async () => {
   //   const easyfetch = new EasyFetch(`${global.BE_HOST}/chat`, "POST");
@@ -23,11 +23,20 @@ const MakeChatRoom: FC = (props): JSX.Element => {
   // };
 
   const [title, setTitle] = useState("");
+  const [type, setType] = useState("public");
 
-  useEffect(() => {
-    // test();
-  }, []);
+  // useEffect(() => {
+  //   test();
+  // }, []);
 
+  const makeChatRoom = () => {
+    console.log("Submmit");
+  };
+
+  const onValueChange = (e) => {
+    setType(e.target.value);
+  }
+  /* TODO: 도대체 어떻게 해야 라디오를 적절하게 사용할 수 있는 것인가 시팔 */
   return (
     <>
       <h2>채팅방 만들기</h2>
@@ -35,12 +44,18 @@ const MakeChatRoom: FC = (props): JSX.Element => {
         <div>채팅방 이름</div>
         <input type="text" value={title} onChange={e => setTitle(e.target.value)}/>
         <div>공개범위</div>
-        <input type="radio" id="mc-public" value="공개방" checked />
-        <label htmlFor="mc-public">공개방</label>
-        <input type="radio" id="mc-protected" value="비공개방" />
-        <label htmlFor="mc-protected">비공개방</label>
-        <input type="radio" id="mc-secret" value="시크릿방" />
-        <label htmlFor="mc-secret">시크릿방</label>
+        <label htmlFor="mc-public">
+          <input type="radio" id="mc-public" value="public" checked={type === "public"} onClick={() => setType("public")} />
+          공개방
+        </label>
+        <label htmlFor="mc-protected">
+          <input type="radio" id="mc-protected" value="protected" checked={type === "protected"} onClick={() => setType("protected")} />
+          비공개방
+        </label>
+        <label htmlFor="mc-secret">
+          <input type="radio" id="mc-secret" value="secret" checked={type === "secret"} onClick={() => setType("secret")} />
+          시크릿방
+        </label>
       </form>
     </>
   );
