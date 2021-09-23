32079f79aac22df8d08d35f4bb599db3f45fb687
Donghyeon Lee <zuzudnf@gmail.com>
2021-09-23 19:39:25 +0900

    geklio

--- app/frontend/src/component/modal/content/chat/MakeChatRoom.tsx
+++ app/frontend/src/component/modal/content/chat/MakeChatRoom.tsx
@@ -1,42 +1,46 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
+import "/src/scss/content/chat/MakeChatRoom.scss";
 import EasyFetch from "../../../../utils/EasyFetch";
 
 const MakeChatRoom: FC = (props): JSX.Element => {
 
   const makeChatRoom = (e) => {
-    console.log("cubsuimt1");
     e.preventDefault();
+    console.log("cubsuimt1");
   };
 
-  const test = async () => {
-    const easyfetch = new EasyFetch(`${global.BE_HOST}/chat`, "POST");
-    const body = {
-      "title": "아무나 와보던가",
-      "type": "public",
-      "passwd": "",
-      "max_people": 10
-    };
-    const res = await (await easyfetch.fetch(body)).json();
+  // const test = async () => {
+  //   const easyfetch = new EasyFetch(`${global.BE_HOST}/chat`, "POST");
+  //   const body = {
+  //     "title": "아무나 와보던가",
+  //     "type": "public",
+  //     "passwd": "",
+  //     "max_people": 10
+  //   };
+  //   const res = await (await easyfetch.fetch(body)).json();
 
-    console.log("res: ", res);
-  };
+  //   console.log("res: ", res);
+  // };
+
+  const [title, setTitle] = useState("");
 
   useEffect(() => {
-    test();
+    // test();
   }, []);
 
   return (
     <>
+      <h2>채팅방 만들기</h2>
       <form onSubmit={makeChatRoom}>
-        <label htmlFor="mc-chat-title">대화방 이름 :</label>
-        <input id="mc-chat-title" type="text"/>
-        <label htmlFor="mc-chat-type">대화방 타입 :</label>
-        <input id="mc-chat-type" type="text"/>
-        <label htmlFor="mc-chat-password">비밀번호: </label>
-        <input id="mc-chat-password" type="text"/>
-        <label htmlFor="mc-chat-max">최대인원: </label>
-        <input id="mc-chat-max" type="number"/>
-        <input type="submit" value="adfs"/>
+        <div>채팅방 이름</div>
+        <input type="text" value={title} onChange={e => setTitle(e.target.value)}/>
+        <div>공개범위</div>
+        <input type="radio" id="mc-public" value="공개방" checked />
+        <label htmlFor="mc-public">공개방</label>
+        <input type="radio" id="mc-protected" value="비공개방" />
+        <label htmlFor="mc-protected">비공개방</label>
+        <input type="radio" id="mc-secret" value="시크릿방" />
+        <label htmlFor="mc-secret">시크릿방</label>
       </form>
     </>
   );
