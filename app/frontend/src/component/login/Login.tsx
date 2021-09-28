6b88f8fdb23029a52080cc347fa5a8a312e7a747
AMATEUR_TOSS <amateur.toss@gmail.com>
2021-09-28 16:09:59 +0900

    Refactor Login.tsx

--- app/frontend/src/component/login/Login.tsx
+++ app/frontend/src/component/login/Login.tsx
@@ -96,7 +96,10 @@ const Login = (): JSX.Element => {
   const UID: string = `1cbdbdbda9ab3cee5773541a19177fa2ae63ae10751dcf3c40466d9a1f0e3f9f`;
 
   const loginOnClick = () => {
-    window.location.href = `https://api.intra.42.fr/oauth/authorize?client_id=${UID}&redirect_uri=http%3A%2F%2F127.0.0.1%2Fsession%2Foauth&response_type=code`;
+    window.location.href = `https://api.intra.42.fr/oauth/authorize`
+    + `?client_id=${UID}`
+    + `&redirect_uri=http%3A%2F%2F127.0.0.1%2Fsession%2Foauth`
+    + `&response_type=code`;
   };
 
   const loginTest1 = () => {
