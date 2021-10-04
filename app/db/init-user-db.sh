13f63fb4faaed118cdff7070d5f5c2a1a3036f8a
Hangyeol Na <hna@c10r4s14.42seoul.kr>
2021-10-04 17:09:02 +0900

    Add dm 더미 데이터

--- app/db/init-user-db.sh
+++ app/db/init-user-db.sh
@@ -12,4 +12,14 @@ psql <<- EOSQL
     CREATE DATABASE $PG_PONG_DB;
     GRANT ALL PRIVILEGES ON DATABASE $PG_PONG_DB TO $PG_PONG_ADMIN;
     CREATE USER pong_session_admin WITH PASSWORD '1234';
+    INSERT INTO dm_store VALUES (5, 'tester01', 'tester02', '1');
+    INSERT INTO dm_store VALUES (6, 'tester01', 'tester02', '2');
+    INSERT INTO dm_store VALUES (7, 'tester02', 'tester01', '3');
+    INSERT INTO dm_store VALUES (8, 'tester01', 'tester02', '4');
+    INSERT INTO dm_store VALUES (9, 'tester02', 'tester01', '5');
+    INSERT INTO dm_store VALUES (10, 'hna', 'tester01', '1');
+    INSERT INTO dm_store VALUES (11, 'tester01', 'hna', '2');
+    INSERT INTO dm_store VALUES (12, 'tester02', 'tester01', '6');
+    INSERT INTO dm_store VALUES (13, 'tester01', 'yochoi', '1');
+    INSERT INTO dm_store VALUES (14, 'jinbkim', 'tester01', '1');
 EOSQL
