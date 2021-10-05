84dc5f3e6699f9687dd7d60d57c15dd31df139d9
Hangyeol Na <hna@c10r4s14.42seoul.kr>
2021-10-05 17:48:08 +0900

    Fix chat socket listener

--- app/backend/src/chat/chat.module.ts
+++ app/backend/src/chat/chat.module.ts
@@ -8,11 +8,15 @@ import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
 import { UsersModule } from 'src/users/users.module';
 import { Admin } from 'src/entities/admin';
+import { GlobalModule } from 'src/global/global.module';
+import { SessionModule } from 'src/session/session.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Users, Chat, ChatUsers, Admin]),
-    forwardRef(() => UsersModule)
+    forwardRef(() => UsersModule),
+    forwardRef(() => GlobalModule),
+    forwardRef(() => SessionModule),
   ],
   controllers: [ChatController],
   providers: [ChatService, ChatGateway],
