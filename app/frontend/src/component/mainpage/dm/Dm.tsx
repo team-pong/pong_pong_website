84f8cc2c25243f36711cf58c50b39a3b46b2729d
AMATEUR_TOSS <amateur.toss@gmail.com>
2021-10-07 16:44:20 +0900

    Add dm detect outside

--- app/frontend/src/component/mainpage/dm/Dm.tsx
+++ app/frontend/src/component/mainpage/dm/Dm.tsx
@@ -1,28 +1,30 @@
-import { FC, useEffect, useRef, useState } from "react";
+import { Dispatch, FC, SetStateAction, useEffect, useRef, useState } from "react";
 import DmList from "./DmList";
 import DmRoom from "./DmRoom";
 import "/src/scss/dm/Dm.scss";
 
-const Dm: FC = (): JSX.Element => {
+const Dm: FC<{setIsDmOpen: Dispatch<SetStateAction<boolean>>}> = ({setIsDmOpen}): JSX.Element => {
 
   const dmRef = useRef<HTMLDivElement>(null);
   const [dmTarget, setDmTarget] = useState("");
 
   return (
-    <div className="dm-container" ref={dmRef}>
-      <div className="top-bar">
-        <span>
-          {
-            dmTarget === ""
-            ? <>개인 메세지</>
-            : <><img
-                  src="/public/arrow.svg"
-                  alt="뒤로가기"
-                  onClick={() => setDmTarget("")} />{dmTarget}</>
-          }
-        </span>
+    <div id="dm-outside" onClick={(e) => {if(e.target === e.currentTarget) setIsDmOpen(false)}}>
+      <div className="dm-container" ref={dmRef}>
+        <div className="top-bar">
+          <span>
+            {
+              dmTarget === ""
+              ? <>개인 메세지</>
+              : <><img
+                    src="/public/arrow.svg"
+                    alt="뒤로가기"
+                    onClick={() => setDmTarget("")} />{dmTarget}</>
+            }
+          </span>
+        </div>
+        {dmTarget === "" ? <DmList setDmTarget={setDmTarget}/> : <DmRoom dmTarget={dmTarget}/>}
       </div>
-      {dmTarget === "" ? <DmList setDmTarget={setDmTarget}/> : <DmRoom dmTarget={dmTarget}/>}
     </div>
   )
 }
