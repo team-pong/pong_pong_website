305fc0acc1d7b58b2b61db23bcbe0060a1cb4170
Hangyeol Na <hna@c10r4s14.42seoul.kr>
2021-10-12 16:24:15 +0900

    Add ladder system 래더 경기시 래더 점수 변동되도록 수정

--- app/backend/src/game/game.gateway.ts
+++ app/backend/src/game/game.gateway.ts
@@ -18,8 +18,6 @@ interface User {
 
 type MatchType = 'normal' | 'ladder';
 
-type MapType = 0 | 1 | 2;
-
 interface MatchInfo {
   lPlayerNickname: string,
   lPlayerAvatarUrl: string,
@@ -29,7 +27,7 @@ interface MatchInfo {
   rPlayerScore: number,
   viewNumber: number,
 	type: MatchType,
-	map: MapType,
+	map: number,
 }
 
 interface socketInfo {
@@ -43,6 +41,8 @@ interface socketInfo {
 interface Game {
 	timeout: NodeJS.Timeout,
 	interval: NodeJS.Timeout,
+	type: MatchType;
+	map: number;
 }
 
 interface GameInfo {
@@ -74,6 +74,7 @@ export class GameGateway {
 	@WebSocketServer()public server: Server;
 
 	private normal_queue: User[] = [];
+	private ladder_queue: User[] = [];
 	private socket_infos: {[key: string]: socketInfo} = {};
 	private games: {[key: string]: Game} = {};
 
@@ -112,44 +113,57 @@ export class GameGateway {
 		}
 	}
 
- GiveUpEventListener (playerLeft: User, playerRight: User, room_id: string, gameLogic: GameLogic, game_type: string, map: number, position: Position) {
+ GiveUpEventListener (playerLeft: User, playerRight: User, gameLogic: GameLogic, position: Position) {
+		const room_id = this.socket_infos[playerLeft.socket.id].rid;
 		const winner = position == 'l' ? playerRight : playerLeft;
 		const loser = position == 'l' ? playerLeft : playerRight;
-		this.matchService.createMatch(winner.id, loser.id, gameLogic._score[1], gameLogic._score[0], 'normal', 0);
+		this.saveResult(winner, loser, gameLogic._score[1], gameLogic._score[0], room_id);
 		loser.socket.emit('matchEnd', 'LOSE');
 		winner.socket.emit('matchEnd', 'WIN');
 		this.clearGame(playerLeft, playerRight, room_id, gameLogic);
 	}
 
-	prepareNextRound(userInfo: MatchInfo, playerLeft: User, playerRight: User, room_id: string, gameLogic: GameLogic) {
+	saveResult(winner: User, loser: User, winner_score: number, loser_score: number, room_id: string) {
+		this.matchService.createMatch(winner.id, loser.id, winner_score, loser_score, this.games[room_id].type, this.games[room_id].map);
+		if (this.games[room_id].type == 'ladder') {
+			this.matchService.updateLadderScore(winner.id, 100);
+			this.matchService.updateLadderScore(loser.id, -100);
+		}
+	}
+
+	prepareNextRound(userInfo: MatchInfo, playerLeft: User, playerRight: User, gameLogic: GameLogic) {
 		gameLogic.initGame();
+		const room_id = this.socket_infos[playerLeft.socket.id].rid;
 		this.server.to(room_id).emit('setMatchInfo', userInfo);
 		clearInterval(this.games[room_id].interval);
 		if (userInfo.lPlayerScore == 3 || userInfo.rPlayerScore == 3 || (userInfo.lPlayerScore + userInfo.rPlayerScore) >= 5) {
-				playerLeft.socket.emit('matchEnd', userInfo.lPlayerScore == 3 ? 'WIN' : 'LOSE');
-				playerRight.socket.emit('matchEnd', userInfo.rPlayerScore == 3 ? 'WIN' : 'LOSE');
+				const winner = userInfo.lPlayerScore == 3 ? playerLeft : playerRight;
+				const loser = userInfo.lPlayerScore == 3 ? playerRight : playerLeft;
+				winner.socket.emit('matchEnd', 'WIN');
+				loser.socket.emit('matchEnd', 'LOSE');
+				this.saveResult(winner, loser, gameLogic._score[1], gameLogic._score[0], room_id);
 				this.clearGame(playerLeft, playerRight, room_id, gameLogic);
 			} else {
 				console.log("timeout start");
 				this.server.to(room_id).emit("startCount");
 				this.games[room_id].timeout = setTimeout(() => {
 					this.games[room_id].interval = setInterval(() => {
-						this.gameInterval(userInfo, playerLeft, playerRight, room_id, gameLogic);
+						this.gameInterval(userInfo, playerLeft, playerRight, gameLogic);
 					}, 20)
 					console.log("timeout end");
 				}, 3000)
 			}
 	}
 
-	gameInterval = (userInfo: MatchInfo, playerLeft: User, playerRight: User, room_id: string, gameLogic: GameLogic) => {
+	gameInterval = (userInfo: MatchInfo, playerLeft: User, playerRight: User, gameLogic: GameLogic) => {
 		if (gameLogic._score == Scored.PLAYER00) {
 			console.log("left win");
 			userInfo.lPlayerScore++;
-			this.prepareNextRound(userInfo, playerLeft, playerRight, room_id, gameLogic);
+			this.prepareNextRound(userInfo, playerLeft, playerRight, gameLogic);
 		} else if (gameLogic._score == Scored.PLAYER01) {
 			console.log("right win");
 			userInfo.rPlayerScore++;
-			this.prepareNextRound(userInfo, playerLeft, playerRight, room_id, gameLogic);
+			this.prepareNextRound(userInfo, playerLeft, playerRight, gameLogic);
 		} else {
 			// frontㅇㅔ서 준비 완료되면 시작(3, 2, 1, Start 메시지)
 			gameLogic.update();
@@ -180,24 +194,39 @@ export class GameGateway {
 		const winner = position == 'l' ? right_player : left_player;
 		const loser = position == 'l' ? left_player : right_player;
 
-		this.matchService.createMatch(winner.id, loser.id, gameLogic._score[0], gameLogic._score[1], 'normal', 0);
+		this.saveResult(winner, loser, gameLogic._score[1], gameLogic._score[0], room_id);
 		winner.socket.emit('matchEnd', 'WIN');
 		this.clearGame(left_player, right_player, room_id, gameLogic);
 	}
 
-	deleteFromList(user_id: string) {
+	deleteFromNormalQueue(user_id: string) {
 		const idx = this.normal_queue.findIndex((element) => {
 			if (element.id = user_id) {
 				return true;
 			}
 			return false;
 		})
-		this.normal_queue.splice(idx, 1);
+		if (idx != -1) {
+			this.normal_queue.splice(idx, 1);
+		}
+	}
+
+	deleteFromLadderQueue(user_id: string) {
+		const idx = this.ladder_queue.findIndex((element) => {
+			if (element.id = user_id) {
+				return true;
+			}
+			return false;
+		})
+		if (idx != -1) {
+			this.ladder_queue.splice(idx, 1);
+		}
 	}
 
   @SubscribeMessage('normal')
   async handleMessage(@ConnectedSocket() socket: Socket, @MessageBody() map: string) {
 		const map_type = Number(map);
+		const game_type = 'normal';
 		// 쿠키에서 sid 파싱
 		const sid: string = this.globalService.getSessionIDFromCookie(socket.request.headers.cookie);
 		// sid로 유저 아이디 찾기
@@ -231,8 +260,8 @@ export class GameGateway {
 			playerRight.socket.join(room_id);
 			playerLeft.socket.emit('matched', {roomId: room_id, opponent: this.normal_queue[1].id, position: 'left'});
 			playerRight.socket.emit('matched', {roomId: room_id, opponent: this.normal_queue[0].id, position: 'right'});
-			this.deleteFromList(waiters[0].id);
-			this.deleteFromList(waiters[1].id);
+			this.deleteFromNormalQueue(waiters[0].id);
+			this.deleteFromNormalQueue(waiters[1].id);
 
 			const ret = gameLogic.getJson();
 			const userInfo: MatchInfo = {
@@ -243,7 +272,7 @@ export class GameGateway {
 				rPlayerAvatarUrl: await this.usersService.getAvatarUrl(playerRight.id),
 				rPlayerScore: 0,
 				viewNumber: 0,
-				type: 'normal',
+				type: game_type,
 				map: 0,
 			}
 			this.socket_infos[playerLeft.socket.id].match = userInfo;
@@ -260,15 +289,15 @@ export class GameGateway {
 			 * @brief 기권 버튼 클릭시 결과 전송 후 게임 종료
 			*/
 			
-			playerLeft.socket.on("giveUp", () => this.GiveUpEventListener(playerLeft, playerRight, room_id, gameLogic, 'normal', 0, 'l'));
-			playerRight.socket.on("giveUp", () => this.GiveUpEventListener(playerLeft, playerRight, room_id, gameLogic, 'normal', 0, 'r'));
+			playerLeft.socket.on("giveUp", () => this.GiveUpEventListener(playerLeft, playerRight, gameLogic, 'l'));
+			playerRight.socket.on("giveUp", () => this.GiveUpEventListener(playerLeft, playerRight, gameLogic, 'r'));
 			console.log("timeout start");
 
-			this.games[room_id] = {timeout: null, interval: null};
+			this.games[room_id] = {timeout: null, interval: null, type: game_type, map: map_type};
 			this.server.to(room_id).emit("startCount");
 			this.games[room_id].timeout = setTimeout(() => {
 				this.games[room_id].interval = setInterval(() => {
-					this.gameInterval(userInfo, playerLeft, playerRight, room_id, gameLogic);
+					this.gameInterval(userInfo, playerLeft, playerRight, gameLogic);
 				}, 20)
 				console.log("timeout end");
 			}, 3000)
@@ -286,8 +315,92 @@ export class GameGateway {
 	
 
 	@SubscribeMessage('ladder')
-	async handdleMessage(@ConnectedSocket() socket: Socket) {
-		
+	async handdleMessage(@ConnectedSocket() socket: Socket, @MessageBody() map: string) {
+		const map_type = Number(map);
+		const game_type = 'ladder';
+		// 쿠키에서 sid 파싱
+		const sid: string = this.globalService.getSessionIDFromCookie(socket.request.headers.cookie);
+		// sid로 유저 아이디 찾기
+		const userid = await this.sessionService.readUserId(sid);
+
+		this.socket_infos[socket.id] = {socket: socket, sid: sid, uid: userid, rid: null, match: null};
+		// 큐에 넣기
+		if (this.ladder_queue.find((element) => {
+			if (element.id == userid) {
+				return true;
+			}
+			return false;
+		})) {
+			console.log('중복 대기열:', userid);
+		} else {
+			this.ladder_queue.push({id: userid, socket: socket, map: map_type})
+		}
+		// 같은 맵을 선택하고 기다리는중인 사람들 리스트
+		const waiters = this.ladder_queue.filter((element) => element.map == map_type);
+		if (waiters.length >= 2) {
+			console.log('매칭 완료');
+			
+			const gameLogic = new GameLogic(700, 450, Number(map), this.server);
+			const playerLeft = waiters[0];
+			const playerRight = waiters[1];
+
+			const room_id: string = playerLeft.id + playerRight.id;
+			this.socket_infos[playerLeft.socket.id].rid = room_id;
+			this.socket_infos[playerRight.socket.id].rid = room_id;
+			playerLeft.socket.join(room_id);
+			playerRight.socket.join(room_id);
+			playerLeft.socket.emit('matched', {roomId: room_id, opponent: this.ladder_queue[1].id, position: 'left'});
+			playerRight.socket.emit('matched', {roomId: room_id, opponent: this.ladder_queue[0].id, position: 'right'});
+			this.deleteFromLadderQueue(waiters[0].id);
+			this.deleteFromLadderQueue(waiters[1].id);
+
+			const ret = gameLogic.getJson();
+			const userInfo: MatchInfo = {
+				lPlayerNickname: playerLeft.id,
+				lPlayerAvatarUrl: await this.usersService.getAvatarUrl(playerLeft.id),
+				lPlayerScore: 0,
+				rPlayerNickname: playerRight.id,
+				rPlayerAvatarUrl: await this.usersService.getAvatarUrl(playerRight.id),
+				rPlayerScore: 0,
+				viewNumber: 0,
+				type: 'ladder',
+				map: 0,
+			}
+			this.socket_infos[playerLeft.socket.id].match = userInfo;
+			this.socket_infos[playerRight.socket.id].match = userInfo;
+
+			this.server.to(room_id).emit("init", gameLogic.getInitJson(), userInfo);
+
+			playerLeft.socket.emit('setMatchInfo', {...userInfo, myName: userInfo.lPlayerNickname} );
+			playerRight.socket.emit('setMatchInfo', {...userInfo, myName: userInfo.rPlayerNickname} );
+			playerLeft.socket.on('keyEvent', (e) => this.BarMovementEventListner(e, gameLogic, 'l'));
+			playerRight.socket.on('keyEvent', (e) => this.BarMovementEventListner(e, gameLogic, 'r'));
+
+			/*
+			 * @brief 기권 버튼 클릭시 결과 전송 후 게임 종료
+			*/
+			
+			playerLeft.socket.on("giveUp", () => this.GiveUpEventListener(playerLeft, playerRight, gameLogic, 'l'));
+			playerRight.socket.on("giveUp", () => this.GiveUpEventListener(playerLeft, playerRight, gameLogic, 'r'));
+			console.log("timeout start");
+
+			this.games[room_id] = {timeout: null, interval: null, type: game_type, map: map_type};
+			this.server.to(room_id).emit("startCount");
+			this.games[room_id].timeout = setTimeout(() => {
+				this.games[room_id].interval = setInterval(() => {
+					this.gameInterval(userInfo, playerLeft, playerRight, gameLogic);
+				}, 20)
+				console.log("timeout end");
+			}, 3000)
+			
+			/*
+			 * 게임 중 연결 끊은 경우
+			 */
+			playerLeft.socket.on("disconnect", () => this.disconnectEvent('l', playerLeft, playerRight, gameLogic));
+			playerRight.socket.on("disconnect", () => this.disconnectEvent('r', playerLeft, playerRight, gameLogic));
+
+		}
+		console.log('대기열 인원 수:', this.ladder_queue.length);
 	}
 
 	afterInit(server: Server): any {
@@ -306,17 +419,8 @@ export class GameGateway {
 		const user_id = await this.sessionService.readUserId(sid);
 
 		console.log('Game 웹소켓 연결해제', user_id);
-		const isSameSocketID = (element) => {
-			if (element.socket.id == socket.id) {
-				return true;
-			}
-			return false;
-		}
-
 		// 대기열에 있다면 대기열에서 제거
-		const idx = this.normal_queue.findIndex(isSameSocketID);
-		if (idx != -1) {
-			this.normal_queue.splice(idx, 1);
-		}
+		this.deleteFromNormalQueue(user_id);
+		this.deleteFromLadderQueue(user_id);
 	}
 }
