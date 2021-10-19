caf3e8625defecc95d5021730ef8a95a7fec2b38
Hangyeol Na <hna@c10r1s14.42seoul.kr>
2021-10-19 16:50:33 +0900

    Add block dm

--- app/backend/src/global/global.gateway.ts
+++ app/backend/src/global/global.gateway.ts
@@ -15,6 +15,7 @@ import { DmStoreService } from 'src/dm-store/dm-store.service';
 import { string } from 'joi';
 import { UseGuards } from '@nestjs/common';
 import { LoggedInWsGuard } from 'src/auth/logged-in-ws.guard';
+import { Block } from 'src/entities/block';
 
 // key: user id / value: socket id
 const socketMap = {};
@@ -43,6 +44,7 @@ export class GlobalGateway {
   constructor(
     @InjectRepository(Users) private usersRepo: Repository<Users>,
     @InjectRepository(Friend) private friendRepo: Repository<Friend>,
+    @InjectRepository(Block) private blockRepo: Repository<Block>,
     private sessionService: SessionService, // readUserId 함수 쓰려고 가져옴
     private friendService: FriendService,
     private globalService: GlobalService,
@@ -78,6 +80,15 @@ export class GlobalGateway {
     }  
   }
 
+  async isBlockedUserFrom(target: string, from: string) {
+    if (await this.blockRepo.count({user_id: from, block_id: target})) {
+      console.log(`${target} is blocked by ${from}`);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   /*!
    * @brief 사용자가 DM을 보내는 경우
    *        1. DB에 저장.
@@ -87,14 +98,17 @@ export class GlobalGateway {
   handleMessage(@ConnectedSocket() socket: Socket, @MessageBody() body: DMSendMsg): any {
     const user_id = findUIDwithSID(socket.id);
     const target_id = body.to;
-    this.dmService.createDmStore(user_id, target_id, body.msg);
-    const target_sid = socketMap[target_id]
-    if (target_sid) {
-      this.server.to(target_sid).emit('dm', {
-        from: user_id,
-        msg: body.msg,
-        time: Date.now(),
-      })
+    if (this.isBlockedUserFrom(user_id, target_id)) {
+    } else {
+      this.dmService.createDmStore(user_id, target_id, body.msg);
+      const target_sid = socketMap[target_id]
+      if (target_sid) {
+        this.server.to(target_sid).emit('dm', {
+          from: user_id,
+          msg: body.msg,
+          time: Date.now(),
+        })
+      }
     }
   }
 
