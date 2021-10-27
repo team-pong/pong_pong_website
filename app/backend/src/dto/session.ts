9ef1fbedcf94385c137af9d271835ffc60ec6439
Hangyeol Na <hna@c10r1s14.42seoul.kr>
2021-10-27 15:29:31 +0900

    Add session validation

--- app/backend/src/dto/session.ts
+++ app/backend/src/dto/session.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsBooleanString, IsNotEmpty, IsString } from "class-validator";
 
 export class SessionDto1{
   @ApiProperty({
@@ -6,4 +7,33 @@ export class SessionDto1{
 		description: '유저 아이디',
 	})
 	public user_id: string;
+}
+
+export class ReadUserWithSessionDto {
+	@ApiProperty({
+		description: '세션 아이디',
+	})
+	@IsString()
+	@IsNotEmpty()
+	public sid: string;
+}
+
+export class UpdateMultiFactorLoginDto {
+	@ApiProperty({
+		example: true,
+		description: '2차 인증 방법중 email 사용 여부'
+	})
+	@IsBooleanString()
+	@IsNotEmpty()
+	public email: boolean;
+}
+
+export class LoginWithEmailCodeDto {
+	@ApiProperty({
+		example: 'asdf',
+		description: '전달받은 이메일 인증 코드'
+	})
+	@IsString()
+	@IsNotEmpty()
+	public code: string;
 }
