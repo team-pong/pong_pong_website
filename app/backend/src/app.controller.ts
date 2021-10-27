4562a87eb20e6ff54520a034969e32c6f0357f84
Hangyeol Na <hna@c10r1s14.42seoul.kr>
2021-10-27 17:58:30 +0900

    Add Multer

--- app/backend/src/app.controller.ts
+++ app/backend/src/app.controller.ts
@@ -1,11 +1,38 @@
-import { Controller, Get, Body, Post, Res, Req, Param, Query, Delete } from '@nestjs/common';
+import { Controller, Get, Body, Post, Res, Req, Param, Query, Delete, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { AppService } from './app.service';
 import { LoginCodeDto } from './dto/login-token-dto';
 import { Request, Response } from 'express';
 import { Cookie } from 'cookiejar';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+
+
+export const randomFileName = (req, file, callback) => {
+  const name = file.originalname.split('.')[0];
+  const fileExtName = extname(file.originalname);
+  const randomName = Array(4).fill(null).map(() => Math.round(Math.random() * 16).toString(16)).join('');
+  callback(null, `${name}-${randomName}${fileExtName}`);
+};
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
   
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', {
+    storage: diskStorage({
+      destination: './files',
+      filename: randomFileName,
+    }),
+  }))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    const response = {
+      originalname: file.originalname,
+      filename: file.filename,
+      path: file.path
+    }
+    console.log(file);
+    return response;
+  }
 }
