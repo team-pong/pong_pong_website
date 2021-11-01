1d97398b4cf087e0afa61c78491416d7073f8665
AMATEUR_TOSS <amateur.toss@gmail.com>
2021-11-01 16:53:36 +0900

    Refactor QuestionContent
    함수 나누고 답변 내역에 따라 questionContent가 다르게 보이도록 수정

--- app/frontend/src/component/mainpage/adminview/question/QuestionContent.tsx
+++ app/frontend/src/component/mainpage/adminview/question/QuestionContent.tsx
@@ -6,10 +6,12 @@ import "/src/scss/content/QuestionContent.scss";
 
 interface Question {
   question_id: number;
-  user_id: string;
+  nick: string;
   title: string;
   email: string;
   content: string;
+  answer: string;
+  question_time: string;
 }
 
 const QuestionContent: FC = (): JSX.Element => {
@@ -17,10 +19,12 @@ const QuestionContent: FC = (): JSX.Element => {
   const [targetAvatar, setTargetAvatar] = useState("");
   const [questionInfo, setQuestionInfo] = useState<Question>({
     question_id: 0,
-    user_id: "",
+    nick: "",
     title: "",
     email: "",
-    content: ""
+    content: "",
+    answer: "",
+    question_time: ""
   })
   const [answerEmail, setAnswerEmail] = useState("");
   const [answerBody, setAnswerBody] = useState("");
@@ -36,7 +40,7 @@ const QuestionContent: FC = (): JSX.Element => {
     const res: Question = await easyfetch.fetch();
 
     /* get inquirer avatar */
-    const easyfetch2 = new EasyFetch(`${global.BE_HOST}/users?nick=${res.user_id}`, "GET");
+    const easyfetch2 = new EasyFetch(`${global.BE_HOST}/users?nick=${res.nick}`, "GET");
     const avatar: string = (await easyfetch2.fetch()).avatar_url;
     setTargetAvatar(avatar);
     setQuestionInfo(res);
@@ -47,53 +51,86 @@ const QuestionContent: FC = (): JSX.Element => {
     getQuestionInfo();
   }, []);
 
-  const sendReply = async (e: FormEvent) => {
-    e.preventDefault();
+  /*!
+   * @brief yochoi
+   * @details 답변완료로 설정하는 함수
+   */
+  const setQuestionAsAnswered = async () => {
+    const easyfetch = new EasyFetch(`${global.BE_HOST}/questions/answer`, "POST");
+    const res: {err_msg: string} = await easyfetch.fetch({
+      question_id: +history.location.pathname.split("/")[3],
+      answer: answerBody
+    });
+    if (res.err_msg !== "에러가 없습니다.") throw (res.err_msg);
+  }
+
+  /*!
+   * @brief yochoi
+   * @details 문의자에게 답변을 보내는 함수
+   *          실패시 에러 문자열을 throw 함
+   */
+  const sendReply = async () => {
     const easyfetch = new EasyFetch(`${global.BE_HOST}/questions/reply`, "POST");
     const res = await easyfetch.fetch({
       email: answerEmail,
       content: answerBody
     });
-    if (res.err_msg || res.statusCode === 400) setNoticeInfo({isOpen: true, seconds: 3, content: "답변 전송에 실패했습니다.", backgroundColor: "#CE4D36"});
-    else setNoticeInfo({isOpen: true, seconds: 3, content: "답변을 성공적으로 보냈습니다.", backgroundColor: "#62C375"});
+    if (res.err_msg || res.statusCode === 400) throw (res.err_msg ? res.err_msg : "에러가 발생했습니다.");
   }
 
-  const deleteQuestion = async (e: FormEvent) => {
-    e.preventDefault();
-    const easyfetch = new EasyFetch(`${global.BE_HOST}/questions`, "DELETE");
-    const res = await easyfetch.fetch({question_id: questionInfo.question_id});
-    setNoticeInfo({isOpen: true, seconds: 3, content: "문의를 삭제했습니다.", backgroundColor: "#62C375"});
+  /*!
+   * @brief yochoi
+   * @details onSubmit 이벤트 발생시 동작하는 함수
+   *          에러가 있을 시 Notice 를 띄움
+   */
+  const onSubmitAnswer = async (e: FormEvent) => {
+    e.preventDefault()
+    try {
+      await sendReply();
+      await setQuestionAsAnswered();
+      setNoticeInfo({seconds: 3, isOpen: true, content: "답변 작성을 완료했습니다.", backgroundColor: "#62C375"})
+    } catch (e) {
+      setNoticeInfo({seconds: 3, isOpen: true, content: e.err_msg, backgroundColor: "#CE4D36"})
+    }
   }
 
   return (
     <div className="question-content">
       <div className="top-bar">
-        <span>문의 상세 및 답변하기</span>
+        {questionInfo.answer === ""
+          ?
+          <span>문의 상세 및 답변하기</span>
+          :
+          <span>문의 및 답변 내역</span>
+        }
       </div>
       <section className="question-info">
         <h1 className="question-info-title">{questionInfo.title}</h1>
         <div className="inquirer-info">
           <img src={targetAvatar} alt="avatar" className="question-info-avatar" />
-          <div className="question-info-from">{questionInfo.user_id}</div>
+          <div className="question-info-from">{questionInfo.nick}</div>
           <div className="question-info-email">{questionInfo.email}</div>
         </div>
         <div className="question-info-content">{questionInfo.content}</div>
       </section>
       <section className="question-answer">
-        <form onSubmit={sendReply}>
+        <form onSubmit={onSubmitAnswer}>
           <label className="email-target-label">이메일: </label>
           <input
             type="text"
             className="email-target"
             value={answerEmail}
+            readOnly
             onChange={(e) => setAnswerEmail(e.target.value)}/>
           <label className="body-label">본문: </label>
-          <textarea rows={50} cols={50}
+          <textarea
+            rows={50}
+            cols={50}
             className="body"
-            value={answerBody}
+            readOnly={questionInfo.answer !== ""}
+            value={questionInfo.answer === "" ? answerBody : questionInfo.answer}
             onChange={(e) => setAnswerBody(e.target.value)}/>
-          <input className="send" type="submit" name="send" value="답변 보내기" />
-          <button className="delete" onClick={deleteQuestion}>문의 삭제하기</button>
+          {questionInfo.answer === "" && <input className="send" type="submit" name="send" value="답변 보내기" />}
         </form>
       </section>
     </div>
