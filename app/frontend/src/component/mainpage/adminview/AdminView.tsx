5a13ae2ac64f0d0105a31b68990e9f10bc0f7d01
Choi Young-jin <amateur.toss@gmail.com>
2021-11-02 15:08:07 +0900

    Improve admin view (#304)
    admin view 스타일 추가
    답변 후 문의 삭제 -> 답변 완료로 변경
    답변 완료 문의들만 볼 수 있는 버튼 추가
    문의 시간 추가

---
+++ app/frontend/src/component/mainpage/adminview/AdminView.tsx
@@ -0,0 +1,76 @@
+import { FC, useEffect, useState } from "react";
+import { Route, RouteComponentProps, withRouter } from "react-router";
+import { Link } from "react-router-dom";
+import EasyFetch from "../../../utils/EasyFetch";
+import Modal from "../../modal/Modal";
+import QuestionContent from "./question/QuestionContent";
+import "/src/scss/adminview/AdminView.scss";
+import QuestionSelector from "./QuestionSelector";
+import Time from "../../../utils/Time";
+
+interface QuestionPrev {
+  question_id: number;
+  title: string;
+  nick: string;
+  question_time: string;
+}
+
+type QuestionType = "answered" | "notAnswered";
+
+const AdminView: FC<RouteComponentProps> = ({match: {path}}): JSX.Element => {
+
+  const [questionType, setQuestionType] = useState<QuestionType>("notAnswered")
+  const [questionList, setQuestionList] = useState<QuestionPrev[]>([]);
+
+  /*!
+   * @author yochoi
+   * @brief 하위 Component 인 QuestionContent 에서 답변 보내기를 한 후에
+   *        AdminView 를 업데이트 하기 위한 State 로, true 냐 false 냐는 전혀 무관함
+   */
+  const [update, setUpdate] = useState(false);
+
+  const getNotAnsweredQuestionList = async () => {
+    const easyfetch = new EasyFetch(`${global.BE_HOST}/questions/beforeAnswerQuestions`, "GET");
+    setQuestionList(await easyfetch.fetch());
+  }
+
+  const getAnsweredQuestionList = async () => {
+    const easyfetch = new EasyFetch(`${global.BE_HOST}/questions/afterAnswerQuestions`, "GET");
+    setQuestionList(await easyfetch.fetch());
+  }
+
+  useEffect(() => {
+    if (questionType === "notAnswered") getNotAnsweredQuestionList();
+    else if (questionType === "answered") getAnsweredQuestionList();
+  }, [questionType]);
+
+  useEffect(() => {
+    if (questionType === "notAnswered") getNotAnsweredQuestionList();
+    else if (questionType === "answered") getAnsweredQuestionList();
+  }, [update])
+
+  return (
+    <div className="admin-view">
+      <QuestionSelector questionType={questionType} setQuestionType={setQuestionType} />
+      <ul className="question-list">
+        {questionList.map((question, idx) => {
+          return (
+            <Link
+              to={`${path}/${question.question_id}`}
+              className="question"
+              key={idx}>
+              <span>제목: {question.title}</span><br/>
+              <span>작성자: {question.nick}</span><br/>
+              <span>작성시점: {new Time(question.question_time).getWholeTime()}</span>
+            </Link>
+          );
+        })}
+      </ul>
+      <Route path={`${path}/:question_id`}>
+        <Modal id={Date.now()} content={<QuestionContent update={update} setUpdate={setUpdate}/>}/>
+      </Route>
+    </div>
+  )
+}
+
+export default withRouter(AdminView);
