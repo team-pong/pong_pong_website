052139b07a7f9f83e9021fc2458d607b8120a9f2
AMATEUR_TOSS <amateur.toss@gmail.com>
2021-11-19 16:07:36 +0900

    Remove useless import

--- app/frontend/src/component/mainpage/adminview/AdminView.tsx
+++ app/frontend/src/component/mainpage/adminview/AdminView.tsx
@@ -1,4 +1,4 @@
-import {FC, useEffect, useLayoutEffect, useRef, useState} from "react";
+import {FC, useEffect, useRef, useState} from "react";
 import { Route, RouteComponentProps, withRouter } from "react-router";
 import { Link } from "react-router-dom";
 import EasyFetch from "../../../utils/EasyFetch";
