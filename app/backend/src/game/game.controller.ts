1b9143b89dd1ff98ad05c8ce7397c05a3ba89b51
Hangyeol Na <hna@c10r3s3.42seoul.kr>
2021-11-23 17:24:49 +0900

    Add reject game

--- app/backend/src/game/game.controller.ts
+++ app/backend/src/game/game.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { RejectGameDto } from 'src/dto/game';
+import { UsersService } from 'src/users/users.service';
+import { GameGateway } from './game.gateway';
+
 
 @Controller('game')
-export class GameController {}
+export class GameController {
+	constructor(
+		private gameGatway: GameGateway,
+		private userService: UsersService,
+	) {}
+
+	@Post('reject')
+	async rejectGame(@Req() req: Request, @Body() body: RejectGameDto) {
+		const target_info = await this.userService.getUserInfoWithNick(body.from);
+		this.gameGatway.rejectGame(target_info.user_id);
+	}
+}
